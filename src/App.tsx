765c038edfefe5fb0a52b3c07a47e02a927a9621
Renato Damázio <renatopdamazio@gmail.com>
2022-09-08 15:45:11 -0300

    Added grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from "react";
-import { calcNauticalMiles, calcKilometers } from "./utils/distance";
 import Map from "./components/map";
 import Search from "./components/seach/Search";
 import DistanceService from "./components/distance/DistanceService";
+import { Box, Grid } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <Search />
-      <Map />
-      <DistanceService />
-    </div>
+    <Box>
+      <Grid container>
+        <Grid item xs={3}>
+          <Search />
+          <DistanceService />
+        </Grid>
+        <Grid item xs={9}>
+          <Map />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
