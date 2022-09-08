b11bb161923f7466e4c55e8d7a07dbf308ca2929
Renato Damázio <renatopdamazio@gmail.com>
2022-09-08 20:45:25 -0300

    Change icon by change transport

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -2,19 +2,64 @@ import React from "react";
 import {
   Stepper,
   Step,
+  StepContent,
   StepLabel,
+  Typography,
 } from "@mui/material";
+
+import { MdTripOrigin} from "react-icons/md";
+
 import { useSelector } from "react-redux";
+
+import {
+  MdDirectionsCar,
+  MdOutlineDirectionsBike,
+  MdDirectionsBus,
+  MdDirectionsWalk,
+} from "react-icons/md";
 
 function Steps() {
   const directions = useSelector((state: any) => state.distances.steps);
+
+  const iconContructor = () => {
+    const icon = directions[0].travel_mode;
+
+    let displayIcon:any = <MdDirectionsCar color="blue"/>;
+
+    switch(icon) {
+      case "DRIVING":
+        displayIcon = <MdDirectionsCar color="blue"/>
+        break;
+      case "BYCICLING":
+        displayIcon = <MdOutlineDirectionsBike/>
+        break;
+      case "TRANSPORT":
+        displayIcon = <MdDirectionsBus/>
+        break;
+      case "WALKING":
+        displayIcon = <MdDirectionsWalk/>
+        break;
+    }
+
+    return displayIcon;
+  }
+
   return (
     directions && (
-      <Stepper orientation="vertical">
+      <Stepper orientation="vertical" >
         {directions.map((direction: any, index: number) => {
           return (
             <Step active={true}>
-              <StepLabel><p dangerouslySetInnerHTML={{__html: direction.instructions}}></p></StepLabel>
+              <StepLabel StepIconComponent={iconContructor}>
+                <p
+                  dangerouslySetInnerHTML={{ __html: direction.instructions }}
+                ></p>
+              </StepLabel>
+              <StepContent>
+                <Typography variant="caption">
+                  {direction.duration.text} - {direction.distance.text}
+                </Typography>
+              </StepContent>
             </Step>
           );
         })}
