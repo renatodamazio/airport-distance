1ab171f6bc736dca5c1c25c8f176f2e9aa0ebb11
Renato Damázio <renatopdamazio@gmail.com>
2022-09-09 15:13:55 -0300

    Added new logo

--- src/components/searchModal/SearchModal.tsx
+++ src/components/searchModal/SearchModal.tsx
@@ -5,13 +5,16 @@ import "./searchStyles.css";
 
 import illustration2 from "../../illustrations/2.jpg";
 import illustration3 from "../../illustrations/3.jpg";
+import logo from "../../illustrations/logo.png";
 
 
 function SearchModal() {
   return (
     <div className="main-search" style={{backgroundImage: `url(${illustration2})`}}>
       <div className="searchContainer">
-          <div className="searchHeader"><Typography>Airport Distance Calculator</Typography></div>
+          <div className="searchHeader">
+            <img src={logo} alt="Airport calculation" title="Calculate the distance between two airports"/>
+          </div>
           <Search />
       </div>
     </div>
