bfaf897c802c7d2f3aff0d8c887949976983b399
Renato Damázio <renatopdamazio@gmail.com>
2022-09-09 15:00:33 -0300

    Added title

--- src/components/searchModal/SearchModal.tsx
+++ src/components/searchModal/SearchModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import Search from "../seach/Search";
 import "./searchStyles.css";
 
@@ -11,6 +11,7 @@ function SearchModal() {
   return (
     <div className="main-search" style={{backgroundImage: `url(${illustration2})`}}>
       <div className="searchContainer">
+          <div className="searchHeader"><Typography>Airport Distance Calculator</Typography></div>
           <Search />
       </div>
     </div>
