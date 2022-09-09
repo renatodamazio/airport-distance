070ea517a319930c77e814f4c523e213395ddc2a
Renato Damázio <renatopdamazio@gmail.com>
2022-09-09 12:03:11 -0300

    Done with stepps color

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -16,35 +16,42 @@ import {
   MdDirectionsWalk,
 } from "react-icons/md";
 
+import { Colors } from "../../theme/colors";
+
 function Steps() {
   const directions = useSelector((state: any) => state.distances.steps);
 
   const iconContructor = () => {
     const icon = directions[0].travel_mode;
 
-    let displayIcon:any = <MdDirectionsCar color="blue"/>;
+    let displayIcon: any = <MdDirectionsCar color={Colors.primary.main} />;
+    let textColor: string = "";
 
-    switch(icon) {
+    switch (icon) {
       case "DRIVING":
-        displayIcon = <MdDirectionsCar color="blue"/>
+        textColor = Colors.primary.main;
+        displayIcon = <MdDirectionsCar color={textColor} />;
         break;
       case "BICYCLING":
-        displayIcon = <MdOutlineDirectionsBike/>
+        textColor = Colors.success.main;
+        displayIcon = <MdOutlineDirectionsBike color={textColor} />;
         break;
       case "TRANSIT":
-        displayIcon = <MdDirectionsBus/>
+        textColor = Colors.error.main;
+        displayIcon = <MdDirectionsBus color={textColor} />;
         break;
       case "WALKING":
-        displayIcon = <MdDirectionsWalk/>
+        textColor = Colors.warning.main;
+        displayIcon = <MdDirectionsWalk color={textColor} />;
         break;
     }
 
     return displayIcon;
-  }
+  };
 
   return (
     directions && (
-      <Stepper orientation="vertical" >
+      <Stepper orientation="vertical">
         {directions.map((direction: any, index: number) => {
           return (
             <Step active={true}>
